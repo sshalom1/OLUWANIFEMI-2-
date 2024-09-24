52c243eba411344fb8d06c5c89b0d14ff73c87d9
sshalom1 <shalomniffy2007@gmail.com>
2024-09-24 23:38:06 +0100

    My Effort

---
+++ CLASSES/EXAMPLE1.JS
@@ -0,0 +1,12 @@
+class Rectangle{
+    constructor(width,height){
+        this.width = width;
+        this.height = height;
+    }
+    calculateArea(){
+   return this.width  * this.height;   
+    }
+}
+let rectangle = new Rectangle (10,5);
+let area = rectangle.calculateArea();
+console.log("The area of the rectangle is : " + area);
